16d73afba8df4b1d385ff267493aea65193bd1ff
samarthsad03 <sam.sadana03@gmail.com>
2024-11-05 17:07:07 +0530

    Write css

---
+++ currency_converter/script.js
@@ -0,0 +1,5 @@
+const fromamountelement = document.querySelector('.amount');
+const convertedamountelement = document.querySelector('.convertedamount');
+const fromcurrencyelement = document.querySelector('.fromCurrency');
+const tocurrencyelement = document.querySelector('tocurrency');
+const resultelement = document.querySelector('result');
